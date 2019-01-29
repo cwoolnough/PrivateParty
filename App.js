7d17e4e29afd2cf690fbd157c432020f1c223a74
Chris <cwoolnoughchamorro@gmail.com>
2019-01-29 17:40:15 +0000

    start of React Native App

---
+++ App.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+
+export default class App extends Component {
+  state = {
+    places: [],
+    selectedPlace: null
+  };
+
+  placeAddedHandler = (placeName) => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat({
+          key: Math.random(), 
+          name: placeName,
+          image: {
+            uri: "https://images.unsplash.com/photo-1517659649778-bae24b8c2e26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1332&q=80"
+          }
+        })
+      };
+    });
+  };
+
+  placeDeletedHandler = () => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  }
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  }
+
+  placeSelectedHandler = (key) => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
+      };
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceDetail 
+          selectedPlace={this.state.selectedPlace} 
+          onItemDeleted={this.placeDeletedHandler} 
+          onModalClosed={this.modalClosedHandler} 
+        />
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList 
+          places={this.state.places} 
+          onItemSelected={this.placeSelectedHandler} 
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: "#fff",
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  }
+});
