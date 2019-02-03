cb49eac5cfc61d89fdb0bb114bd59a6ea1735880
Chris <cwoolnoughchamorro@gmail.com>
2019-02-03 17:47:16 +0000

    added googlemap

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Image, Button, Text, StyleSheet} from 'react-native';
+import { View, Image, Button, Text, StyleSheet, Dimensions} from 'react-native';
+import MapView from "react-native-maps";
 
 class PickLocation extends Component {
+    state = {
+        focusedLocation: {
+            latitude: 37.7900352,
+            longitude: -122.4013726,
+            latitudeDelta: 0.0122,
+            longitudeDelta: Dimensions.get("window").width / Dimensions.get("window").height * 0.0122
+        }
+    }
+
+
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.placeholder}>
-                    <Text>Map</Text>
-                </View>
+                <MapView 
+                    initialRegion={this.state.focusedLocation}
+                    style={styles.map}
+                />
                 <View style={styles.button}>
                     <Button title="Locate Me" onPress={() => alert('pick location')}/>
                 </View>
@@ -21,12 +33,9 @@ const styles = StyleSheet.create({
         width: "100%",
         alignItems: "center"
     },
-    placeholder: {
-        borderWidth: 1,
-        borderColor: "black",
-        backgroundColor: "#eee",
-        width: "80%",
-        height: 150
+    map: {
+        width: "100%",
+        height: 250
     },
     button: {
         margin: 8
